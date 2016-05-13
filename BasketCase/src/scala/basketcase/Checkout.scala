42d9da7facea9d92edd49475cd353bd5c3df1179
principessamia <deeple_patel@hotmail.com>
2016-05-13 01:49:53 +0100

    Add files via upload

--- BasketCase/src/scala/basketcase/Checkout.scala
+++ BasketCase/src/scala/basketcase/Checkout.scala
@@ -1,10 +1,17 @@
 package scala.basketcase
 
+import java.text.NumberFormat
+
 object Checkout {
   
-  def totalCost(shoppingBasket: ShoppingBasket): BigDecimal = {
+    def totalCost(shoppingBasket: ShoppingBasket): BigDecimal = {
       // Collect prices for all fruits in the basket
       val prices = shoppingBasket.fruits.map(x => x.price);      
-      return prices.sum;
+      val total = prices.sum;
+      
+      // Here, when we output to local currency, we will lose the precision 
+      // Could always write a custom formatter in the future  
+      println("Total cost of the fruit basket is " + NumberFormat.getCurrencyInstance.format(total));
+      return total;
   }
 }
--- BasketCase/src/scala/basketcase/ShoppingBasket.scala
+++ BasketCase/src/scala/basketcase/ShoppingBasket.scala
@@ -5,34 +5,35 @@ import scala.collection.mutable.ArrayBuffer
 
 case class ShoppingBasket(fruits: ArrayBuffer[Fruit])
 {
+    // Nice to haves
     def empty() { this.fruits.clear() }   
     def remove(fruit:Fruit) { fruits -= fruit }
 }
 
-// use a sealed trait - the subtypes are finite in the given exercise
+// Use a sealed trait - the subtypes are finite in the given exercise
 sealed trait Fruit {
   val price: BigDecimal
 }
 
-// Dealing with money, albeit in small amounts, so use BigDecimal to be safe
+// Dealing with money, albeit in small amounts, so use BigDecimal for precision
 final object Banana extends Fruit {
-  val price = BigDecimal(0.30)
+  val price = BigDecimal("0.30")
 }
 
 final object Orange extends Fruit {
-  val price = BigDecimal(0.50)
+  val price = BigDecimal("0.50")
 }
 
 final object Apple extends Fruit {
-  val price = BigDecimal(0.20)
+  val price = BigDecimal("0.20")
 }
 
 final object Lemon extends Fruit {
-  val price = BigDecimal(0.15)
+  val price = BigDecimal("0.15")
 }
 
 final object Peach extends Fruit {
-  val price = BigDecimal(0.75)
+  val price = BigDecimal("0.75")
 }
 
 
